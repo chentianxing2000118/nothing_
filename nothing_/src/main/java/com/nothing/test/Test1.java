5f8699f3d0a868f42389f5fe82ddfb5c3848be4f
paper <1650370911@qq.com>
2019-07-10 20:22:47 +0800

    第一个项目

---
+++ nothing_/src/main/java/com/nothing/test/Test1.java
@@ -0,0 +1,26 @@
+/**   
+ * Copyright © 2019 公司名. All rights reserved.
+ * 
+ * @Title: Test1.java 
+ * @Prject: nothing_
+ * @Package: com.nothing.test 
+ * @Description: TODO
+ * @author: 陈天星  
+ * @date: 2019年7月10日 下午8:20:51 
+ * @version: V1.0   
+ */
+package com.nothing.test;
+
+/** 
+ * @ClassName: Test1 
+ * @Description: TODO
+ * @author:陈天星
+ * @date: 2019年7月10日 下午8:20:51  
+ */
+public class Test1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+	
+}
